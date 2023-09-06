77454ba34beece8079ff0de3a10fd10dd9b0ed94
Javier Ubillos <javier@ubillos.org>
2023-09-06 21:46:45 +0200

    Initial commit auto-indentation.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';const app = express();
-const port = 9000;app.get('/', (req, res) => {
-    res.send('Server is up and running!');
-});app.listen(port, () => {
-    console.log(`Server is listening on ${port}`);
-});
+import express from 'express'; const app = express()
+
+const port = 9000; app.get('/', (req, res) => {
+    res.send('Server is up and running! 🌞');
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on ${port} ℹ️`)
+})
