f066a6d237e67c409d12c3b3db60c416ce68f6a3
Javier Ubillos <javier@ubillos.org>
2023-09-06 21:59:03 +0200

    Minor formatting fix to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express'; const app = express()
 
-const port = 9000; app.get('/', (req, res) => {
+const port = 9000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
     res.send('Server is up and running! 🌞');
 })
 
