65bf972f94adbadb4b845023ab3e81fcfcf0d7d3
Javier Ubillos <javier@ubillos.org>
2023-09-15 00:51:30 +0200

    Even more basic netlify setup.

--- netlify/functions/root.ts
+++ netlify/functions/root.ts
@@ -5,10 +5,8 @@ import publicRouter from '../../src/routes/public';
 
 const app = express();
 
-const router = Router();
 
-router.get('/hello', (req, res) => res.send('Hello World!'));
+app.get('/hello', (req, res) => res.send('Hello World!'));
 
-app.use('/api/', router);
 
 export const handler = serverless(app);
