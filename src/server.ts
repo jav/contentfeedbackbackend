2a59517b01ec084811dfad6b40bce82b066071ca
Javier Ubillos <javier@ubillos.org>
2023-09-14 22:38:47 +0200

    Add getReviwers and refactor getting feedback.

--- src/server.ts
+++ src/server.ts
@@ -15,68 +15,107 @@ app.use(express.static('public'))
 app.use(cors())
 app.use(express.json())
 
-type Reviewer = {
-    description: string
+
+
+type ReviewerFeedback = {
+    name: string,
+    score: number,
+    feedback: string,
+    improvementSuggestion: string
 }
 
 const reviewersRoster = {
     storyTeller: {
-        description: "reviews the text from a story telling perspective. How well does the text tell a story? Does include cliffhangers?"
+        slug: "storyTeller",
+        name: "The Story Teller",
+        description: "A storyteller that reviews the text from a story telling perspective. How well does the text tell a story? Does include cliffhangers?"
     },
     teacher: {
-        description: "reviews the text from an educational perspective. How well does the text teach the reader? Is it clear? Does it include examples?"
+        slug: "teacher",
+        name: "The Teacher",
+        description: "A teacher that reviews the text from an educational perspective. How well does the text teach the reader? Is it clear? Does it include examples?"
     },
     hemingway: {
-        description: "reviews the text from a Hemmingway perspective. How easy is the text to read? Does it concrete, specific and more commonly found words?"
+        slug: "hemingway",
+        name: "Ernst Hemingway",
+        description: "Ernst Hemingway. Hemingway reviews the text from a Hemingway perspective. How easy is the text to read? Does it concrete, specific and more commonly found words?"
     },
     scientist: {
-        description: "reviews the text from a scientific perspective. How well does the text explain the topic? Does it include references? Is it told in an objective way?"
+        slug: "scientist",
+        name: "The Scientist",
+        description: "Reviews the text from a scientific perspective. How well does the text explain the topic? Does it include references? Is it told in an objective way?"
     }
 }
 
-const getFeedbackOnContent = async (contentToGetFeedbackOn: string, selectedReviewers?: string[]) => {
+const getFeedbackOnContent = async (reviewerSlug: string, contentToGetFeedbackOn: string): Promise<ReviewerFeedback> => {
 
-    selectedReviewers = selectedReviewers ?? Object.keys(reviewersRoster)
-    selectedReviewers = selectedReviewers.filter(reviewer => Object.keys(reviewersRoster).includes(reviewer))
-
-    const systemInstructionsPersonas = selectedReviewers.map(
-        reviewer => `The ${reviewer} persona, who ${reviewersRoster[reviewer].description}`
-    ).join('\n')
+    if (!(reviewerSlug in reviewersRoster)) {
+        throw new Error('Invalid reviewerSlug')
+    }
 
     const systemInstructions = `
-    You will be provided with a text, and your task is to
-    through different personas evaluate the quality of the text.
-    You will answer with an json object, where the each persona is a key,
-    and the value is an object with the properties 'score' and 'feedback'.
-    The personas are the following:
-    ${systemInstructionsPersonas}
-    `
+    You will be provided with a text, and your task is to evaluate the text
+    through the perspective of a persona.
+    You will answer with an json object with the following keys and values .
+    Key: score, Value: a number between 0 and 10, where 0 is the worst and 10 is the best.
+    Key: feedback, Value: a string where you describe what you thought about the text from your perspective.
+    Key: improvementSuggestion, Value: a string, where you suggest how the text could be improved from your perspective.
 
-    const gptResponse = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {
-                "role": "system",
-                "content": systemInstructions
-            },
-            {
-                "role": "user",
-                "content": contentToGetFeedbackOn
-            }
-        ],
-        temperature: 0,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-    })
-    return gptResponse.choices[0].message.content
+    Your persona is:
+    ${reviewersRoster[reviewerSlug].description}
+    `
+    try {
+        const gptResponse = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    "role": "system",
+                    "content": systemInstructions
+                },
+                {
+                    "role": "user",
+                    "content": contentToGetFeedbackOn
+                }
+            ],
+            temperature: 0,
+            max_tokens: 512,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        })
+
+        // TODO: Validate output
+
+        const contentObj = JSON.parse(gptResponse.choices[0].message.content)
+        const retFeedback: ReviewerFeedback = {
+            name: reviewersRoster[reviewerSlug].name,
+            score: contentObj.score,
+            feedback: contentObj.feedback,
+            improvementSuggestion: contentObj.improvementSuggestion
+        }
+        return retFeedback
+
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
 }
 
-app.post('/api/getFeedback', async (req: Request, res: Response) => {
-    console.log(req.body)
-    const feedback: string = await getFeedbackOnContent(req.body.contentToGetFeedbackOn)
-    res.json({ feedback })
+app.get('/api/reviewers', async (req: Request, res: Response) => {
+    const reviewersRosterAsList = Object.values(reviewersRoster)
+    return res.json(reviewersRosterAsList)
+})
+
+app.post('/api/reviewers/:reviewerSlug/getFeedback', async (req: Request<{ reviewerSlug: string }>, res: Response) => {
+    const reviewerSlug = req.params.reviewerSlug
+    const contentToGetFeedbackOn = req.body.contentToGetFeedbackOn
+
+    try {
+        const feedback = getFeedbackOnContent(reviewerSlug, contentToGetFeedbackOn)
+        return res.json(feedback)
+    } catch (error) {
+        return res.status(500).json({ error })
+    }
 })
 
 app.listen(port, () => {
