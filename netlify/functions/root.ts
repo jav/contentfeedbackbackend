66f02515d959844a852626fa362122d014a12b74
Javier Ubillos <javier@ubillos.org>
2023-09-15 00:48:19 +0200

    Try chaingg /api/ for /

--- netlify/functions/root.ts
+++ netlify/functions/root.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
 import serverless from 'serverless-http';
 import apirRouter from '../../src/routes/api';
-import publicRouter from '../../src//routes/public';
+import publicRouter from '../../src/routes/public';
 
 const app = express();
 
